d0e3611f209f4fac0cca7679eb38d1edb373ed52
derekjpeters <derek.j.peters@gmail.com>
2024-08-01 17:51:08 -0400

    added app.js

--- app.js
+++ app.js
@@ -3,3 +3,20 @@ const app = express();
 const port = 3000;
 
 //Middleware to parse the JSON data 
+app.use(express.json());
+
+//Import the routers
+const instrumentsRouter = require('./routes/instruments');
+const artistsRouter = require('./routes/artists');
+
+//Use the routers
+app.use('/instruments', instrumentsRouter);
+app.use('/artists', artistsRouter);
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Music Instrument Manager API');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
